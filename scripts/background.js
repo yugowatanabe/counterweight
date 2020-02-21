2c31841d4bdc649a93616ed40d99dc24676205db
Eric Perez <eric9650@MacBook-Pro-3.local>
2020-02-20 20:35:56 -0800

    Moved the export button to the right click menu of icon

--- scripts/background.js
+++ scripts/background.js
@@ -1,3 +1,5 @@
+var debug = false;
+
 // function converting CSV text to JSON object
 function csv_to_json(text){
   var lines = text.split("\n");
@@ -80,24 +82,34 @@ chrome.runtime.onInstalled.addListener(function(details) {
     contexts: ["selection"]  // ContextType
   });
 
-  chrome.contextMenus.onClicked.addListener(function(event) {
-    // Gives an option for custom text search on 'right click'
-    bg = chrome.extension.getBackgroundPage();
-    // Log the selected text
-    if (bg) {
-      bg.console.log("Selected Text: %s", event.selectionText);
-    }
-    // Keep the highlighted text and flag chrome storage
-    chrome.storage.local.get(["highlightedText"], function(res) {
-      chrome.storage.local.set({"highlightedText": event.selectionText});
+  chrome.contextMenus.create({
+    id: "export_button",
+    title: "Export data",
+    contexts: ["page_action"]
+  });
 
-      // Record Time of Icon Click
-      chrome.storage.local.get(["text_selects"], function(res) {
-        texts = res["text_selects"];
-        texts.push([Date(), event.selectionText]);
-        chrome.storage.local.set({"text_selects": texts});
+  chrome.contextMenus.onClicked.addListener(function(event) {
+    if (event.menuItemId == "customTextInput") {
+      // Gives an option for custom text search on 'right click'
+      bg = chrome.extension.getBackgroundPage();
+      // Log the selected text
+      if (bg) {
+        bg.console.log("Selected Text: %s", event.selectionText);
+      }
+      // Keep the highlighted text and flag chrome storage
+      chrome.storage.local.get(["highlightedText"], function(res) {
+        chrome.storage.local.set({"highlightedText": event.selectionText});
+
+        // Record Time of Icon Click
+        chrome.storage.local.get(["text_selects"], function(res) {
+          texts = res["text_selects"];
+          texts.push([Date(), event.selectionText]);
+          chrome.storage.local.set({"text_selects": texts});
+        });
       });
-    });
+    } else if (event.menuItemId == "export_button") {
+      print_data();
+    }
   });
 });
 
@@ -123,3 +135,44 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     });
   }
 });
+
+function print_data() {
+  chrome.storage.local.get(["click_times", "clicked_links", "hovered_ticks", "text_selects", "news_sites_visited"], function(res) {
+    var text = 'event,time,url\n';
+
+    // click_times: Array of Times
+    res["click_times"].forEach(function(item) {
+      text += "click," + item.toString() + ",\n";
+    })
+
+    // clicked_links: Array of Arrays [time, url]
+    res["clicked_links"].forEach(function(item) {
+      text += "link," + item[0].toString() + "," + item[1].toString() + "\n";
+    })
+
+    // hovered_ticks: Array of Arrays [time, tick]
+    res["hovered_ticks"].forEach(function(item) {
+      text += "tick," + item[0].toString() + "," + item[1].toString() + "\n";
+    })
+
+    // text_selects: Array of Arrays [time, text]
+    res["text_selects"].forEach(function(item) {
+      text += "text_select," + item[0].toString() + "," + item[1].toString() + "\n";
+    })
+
+    // news_sites_visited: Array of Arrays [time, text]
+    res["news_sites_visited"].forEach(function(item) {
+      text += "news_sites_visited," + item[0].toString() + "," + item[1].toString() + "\n";
+    })
+
+    bg = chrome.extension.getBackgroundPage();
+    if (bg && debug) {
+      bg.console.log(text);
+    }
+
+    // Write data to file
+    var b = new Blob([text], {type: "text/plain"});
+    var u = URL.createObjectURL(b);
+    chrome.downloads.download({url: u, filename: "data"});
+  });
+}
