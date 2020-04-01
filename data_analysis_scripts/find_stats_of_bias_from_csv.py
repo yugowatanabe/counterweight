30adacbb523b251eb6a083c1e1e38bd1c72e61d7
Eric Perez <eric9650@MacBook-Pro-3.local>
2020-03-31 17:37:48 -0700

    Renaming and moving files

---
+++ data_analysis_scripts/find_stats_of_bias_from_csv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+from tldextract import extract
+
+# Populate a dictionary with the sources and their corresponding bias
+sources = open("csv/sources_histogram.csv","r")
+sources_bias = {}
+highest = 0
+lowest = 999
+for line in sources:
+    current = line.split(',')
+    current_source = current[0].replace(" ", "").lower()
+    current_bias = float(current[1])
+    if current_bias < lowest:
+        lowest = current_bias
+    if current_bias > highest:
+        highest = current_bias
+
+print("highest: " + str(highest) + "\n")
+print("lowest: " + str(lowest) + "\n")
